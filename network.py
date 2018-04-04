fa63fdee15fbd1915c8424099e92c15fa23cf92d
Icoops <tristan.celor@etu.u-bordeaux.fr>
2018-04-04 20:49:06 +0200

    finish error already use

--- network.py
+++ network.py
@@ -18,13 +18,15 @@ class NetworkServerController:
         self.map_name = map_file
         self.clients = []
         self.nick_to_client = {}
-        self.str_character = []
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.s.bind(('', port))
         self.s.listen(1)
 
     # time event
+    def send_all(self, clients, data):
+        for client in clients:
+            client.send(data)
 
     def tick(self, dt):
         demande, b, c = select.select([self.s] + self.clients,[],[],1)
@@ -32,16 +34,6 @@ class NetworkServerController:
             if client == self.s:
                 co, addr = self.s.accept()
                 self.clients.append(co)
-                print("ok")
-                data = self.map_name
-                data = data.encode()
-                co.send(data)
-                data = co.recv(1500)
-                data = data.decode()
-                if data == "ok":
-                    data = pickle.dumps(self.model.fruits)
-                    data = data.encode()
-                    co.send(data)
             else:
                 data = client.recv(1500)
                 data = data.decode()
@@ -53,7 +45,17 @@ class NetworkServerController:
                     client.close()
                 if (commands[0] == "#nickname"):
                     self.nick_to_client[client] = commands[1]
-                    self.model.add_character(commands[1], isplayer = True)
+                    self.model.add_character(commands[1], isplayer = False)
+                    self.send_all(self.clients,pickle.dumps(self.model))
+                if (commands[0] == "#give_me_map"):
+                    data = pickle.dumps(self.model)
+                    client.send(data)
+                if (commands[0] == "#move"):
+                    self.model.move_character(commands[1], int(commands[2]))
+                    self.send_all(self.clients,pickle.dumps(self.model))
+                if (commands[0] == "#bomb"):
+                    self.model.drop_bomb(commands[1])
+                    self.send_all(self.clients,pickle.dumps(self.model))
         return True
 
 ################################################################################
@@ -66,24 +68,17 @@ class NetworkClientController:
         self.host = host
         self.port = port
         self.nickname = nickname
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.connect((host, port))
-        data = self.socket.recv(1500)
-        data = data.decode()
-        self.model = Model()
-        self.model.load_map(data)
-        data = "ok"
-        self.socket.send(data.encode())
-        data = self.socket.recv(1500)
-        data = data.decode()
-        data = pickle.loads(data)
-        print(data)
-        for fruit in data :
-            self.model.add_fruit(fruit.kind, fruit.pos)
-
-
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.s.bind(('', port))
+        self.s.listen(1)
         data = "#nickname " + nickname
         self.socket.send(data.encode())
+        data = self.socket.recv(1500)
+        self.model = pickle.loads(data)
+        for char in self.model.characters:
+            if (char.nickname == self.nickname):
+                self.model.player = char
 
     # keyboard events
 
@@ -93,16 +88,35 @@ class NetworkClientController:
 
     def keyboard_move_character(self, direction):
         print("=> event \"keyboard move direction\" {}".format(DIRECTIONS_STR[direction]))
-        # ...
+        if not self.model.player: return True
+        data = "#move {} {}".format(self.nickname, direction)
+        self.socket.send(data.encode())
         return True
 
     def keyboard_drop_bomb(self):
         print("=> event \"keyboard drop bomb\"")
-        # ...
+        if not self.model.player: return True
+        data = "#bomb {}".format(self.nickname)
+        self.socket.send(data.encode())
         return True
 
     # time event
 
     def tick(self, dt):
-        # ...
+        demande, b, c = select.select([self.socket],[],[],1)
+        for client in demande:
+            if client == self.socket:
+                co, addr = self.socket.accept()
+            else:
+                try:
+                    data = self.socket.recv(1500)
+                    try:
+                        self.model = pickle.loads(data)
+                        for char in self.model.characters:
+                            if (char.nickname == self.nickname):
+                                self.model.player = char
+                    except EOFError:
+                        return True;
+                except socket.error:
+                    return True
         return True
